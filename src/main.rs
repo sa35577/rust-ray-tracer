93ab4c40846e2a0b0f5cfcf3901d27651a5ce07d
sa35577 <s97arora@uwaterloo.ca>
2025-05-11 19:43:55 -0700

    start stuff

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+mod color;
+mod vec3;
+
+use color::*;
+use vec3::*;
+
+fn main() {
+    // Image
+    let image_width = 256;
+    let image_height = 256;
+
+    // Render
+    println!("P3\n{} {}\n255", image_width, image_height);
+
+    for j in 0..image_height {
+        eprintln!("\rScanlines remaining: {}", image_height - j);
+        for i in 0..image_width {
+            // let r = -(i as f64) / ((image_width - 1) as f64);
+            // let g = -(j as f64) / ((image_height - 1) as f64);
+            // let b = 0.0;
+
+            // let ir = (255.999 * r) as i32;
+            // let ig = (255.999 * g) as i32;
+            // let ib = (255.999 * b) as i32;
+
+            // println!("{} {} {}", ir, ig, ib);
+            let r = Color::new(i as f64 / (image_width - 1) as f64, j as f64 / (image_height - 1) as f64, 0.0);
+            write_color(&mut std::io::stdout(), r);
+        }
+    }
+    eprintln!("Done.");
+}
---
+++ src/vec3.rs
@@ -0,0 +1,136 @@
+use std::ops::Add;
+use std::ops::Neg;
+use std::ops::Index;
+use std::ops::IndexMut;
+use std::ops::Sub;
+use std::ops::Mul;
+use std::ops::Div;
+
+#[derive(Copy, Clone)]
+pub struct Vec3 {
+    e: [f64; 3],
+}
+
+pub type Point3 = Vec3;
+
+impl Vec3 {
+    pub fn new(e0: f64, e1: f64, e2: f64) -> Vec3 {
+        Vec3 { e: [e0, e1, e2] }
+    }
+
+    pub fn x(&self) -> f64 {
+        self.e[0]
+    }
+
+    pub fn y(&self) -> f64 {
+        self.e[1]
+    }
+
+    pub fn z(&self) -> f64 {    
+        self.e[2]
+    }
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        self.dot(self)
+    }
+
+    pub fn dot(&self, other: &Vec3) -> f64 {
+        self.e[0] * other.e[0] + self.e[1] * other.e[1] + self.e[2] * other.e[2]
+    }
+
+    pub fn cross(&self, other: &Vec3) -> Vec3 {
+        Vec3::new(
+            self.y() * other.z() - self.z() * other.y(),
+            self.z() * other.x() - self.x() * other.z(),
+            self.x() * other.y() - self.y() * other.x()
+        )
+    }
+
+    pub fn unit_vector(&self) -> Vec3 {
+        *self / self.length()
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, other: Vec3) -> Vec3 {
+        Vec3::new(self.x() + other.x(), self.y() + other.y(), self.z() + other.z())
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Vec3;
+
+    fn neg(self) -> Vec3 {
+        Vec3::new(-self.x(), -self.y(), -self.z())
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, other: Vec3) -> Vec3 {
+        Vec3::new(self.x() - other.x(), self.y() - other.y(), self.z() - other.z())
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: f64) -> Vec3 {
+        Vec3::new(self.x() * rhs, self.y() * rhs, self.z() * rhs)
+    }
+
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f64) -> Vec3 {
+        Vec3::new(self.x() / rhs, self.y() / rhs, self.z() / rhs)
+    }
+}
+
+impl Index<usize> for Vec3 {
+    type Output = f64;
+
+    fn index(&self, i: usize) -> &f64 {
+        &self.e[i]
+    }
+}
+
+impl IndexMut<usize> for Vec3 {
+    fn index_mut(&mut self, i: usize) -> &mut f64 {
+        &mut self.e[i]
+    }
+}
+
+fn main() {
+    let mut v = Vec3::new(1.0, 2.0, 3.0);
+    println!("Original: {} {} {}", v[0], v[1], v[2]);
+    
+    // Demonstrate mutable indexing
+    v[0] = 4.0;
+    println!("After modification: {} {} {}", v[0], v[1], v[2]);
+    
+    let neg_v = -v;
+    println!("Negated: {} {} {}", neg_v[0], neg_v[1], neg_v[2]);
+
+    let v1 = Vec3::new(1.0, 2.0, 3.0);
+    let v2 = Vec3::new(4.0, 5.0, 6.0);
+    let v3 = v1 + v2;
+    println!("Addition: {} {} {}", v3[0], v3[1], v3[2]);
+
+    let v4 = v1 * 2.0;
+    println!("Multiplication: {} {} {}", v4[0], v4[1], v4[2]);
+
+    let v5 = v1 / 2.0;
+    println!("Division: {} {} {}", v5[0], v5[1], v5[2]);
+
+    
+}
